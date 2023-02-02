95985eb3be638992008443d262b614bf280b3c2e
Tanvi Kapurwan <59108224+tanvikapurwan@users.noreply.github.com>
2023-02-02 22:28:48 +0530

    Create main.js

---
+++ operators and expressions in Js/main.js
@@ -0,0 +1,50 @@
+//"Operators in Js"
+
+console.log("Operators in Js");
+
+//Arithematic Operators
+let a = 10;
+let b = 4;
+console.log("a + b =", a+b);
+console.log("a - b =", a-b);
+console.log("a / b =", a/b);
+console.log("a * b =", a*b);
+console.log("a ** b =", a**b);
+console.log("a % b =", a%b);
+console.log("++a", ++a);
+console.log("a++", a++);
+console.log("--a =", --a);
+console.log("a--", a--);
+console.log("a =", a);
+console.log("a--", a--);
+
+//Assignment Operators
+let assignment = 1;
+assignment += 5;
+console.log(assignment);
+
+
+//Comparison Operators
+
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2 is", comp1 == comp2);
+console.log("comp1 != comp2 is", comp1 != comp2);
+console.log("comp1 === comp2 is", comp1 === comp2);
+console.log("comp1 !== comp2 is", comp1 !== comp2);
+console.log("comp1 > comp2 is", comp1 > comp2);
+
+
+//Logical Operator
+let x = 5;
+let y = 6;
+console.log(x<y && x==5);
+console.log(x>y || x==5);
+console.log(!false);
+console.log(!true);
+
+
+// Comments in Javascript
+// This is a single line comment
+/*This is a multiline line Comment */
+

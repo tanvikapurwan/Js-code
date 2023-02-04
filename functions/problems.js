0c07b1bde01f2c1e5621e83e43de1f321633e7ab
Tanvi Kapurwan <59108224+tanvikapurwan@users.noreply.github.com>
2023-02-04 15:53:04 +0530

    Create problems.js

---
+++ functions/problems.js
@@ -0,0 +1,32 @@
+// let marks = {
+//     shubham: 88,
+//     lovish: 23,
+//     monika: 4,
+//     riya: 23,
+//     Tanvi: 98
+// }
+// // Problem no. 1
+// for(let i=0; i<Object.keys(marks).length; i++){
+//     console.log("The marks of "+ Object.keys(marks)[i] + " are "+marks[Object.keys(marks)[i]]);
+// }
+
+// //providing keys
+
+// // Problem no. 2
+
+//Problem no.3
+// let cn = 43;
+// let i;
+// while(i != cn){
+//     i = prompt("Enter a number");
+//     console.log("Try again");
+// }
+// console.log("you have entered a correct number");
+
+// problem 4
+// function for mean
+
+const mean = (a, b, c, d) => {
+    return (a+b+c+d)/4
+}
+console.log(mean(4,5,6,7));

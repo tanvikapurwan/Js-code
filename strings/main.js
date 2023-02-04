72ea5b2ed1b61cc6aaf9ed60f07339c3ac54bdcf
Tanvi Kapurwan <59108224+tanvikapurwan@users.noreply.github.com>
2023-02-04 16:06:54 +0530

    Create main.js

---
+++ strings/main.js
@@ -0,0 +1,30 @@
+let name = "Tanvi";
+let name1 = 'Tanvi';
+console.log(name);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[4]);
+console.log("Length ",name.length);
+
+
+
+// alt + shift + bottom key
+
+let girl = "Riya";
+let boy = "Rahul";
+
+let sentence = '${girl} is a friend of ${boy}';
+console.log(sentence);
+
+//Escape Sequence Characters
+
+let fruit = "Bana\n na";
+console.log(fruit.length);
+console.log(fruit);
+
+let fruit2 = 'Bana\n na';
+console.log(fruit2.length);
+console.log(fruit2);
+

8abf1f4ac6dfd84a9c2b94b0d4538ee41f1e3c1f
Tanvi Kapurwan <59108224+tanvikapurwan@users.noreply.github.com>
2023-02-04 14:16:29 +0530

    Create main.js

---
+++ while and do while loop/main.js
@@ -0,0 +1,19 @@
+// //while loop
+let num = prompt("Enter the value of num");
+num = Number.parseInt(num);
+let j = 0;
+while (j < num) {
+    console.log(j + 1);
+    j++;
+}
+
+//do while loop
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < n)
+
+

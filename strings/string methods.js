dd5ec36f6b6e6d7035c417fec8ca4e317e0fedec
Tanvi Kapurwan <59108224+tanvikapurwan@users.noreply.github.com>
2023-02-04 16:27:37 +0530

    Create string methods.js

---
+++ strings/string methods.js
@@ -0,0 +1,37 @@
+// String length
+// String slice()
+// String substring()
+// String substr()
+// String replace()
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String charAt()
+// String charCodeAt()
+// String split()
+// \n newline  \t tab \ carriage return
+
+let name = "Tanvi Kapurwan";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2,4));
+console.log(name.slice(2));
+console.log(name.replace("Kap", "pak"));
+let friend = "simran";
+console.log(name.concat(" is a friend of ", friend, " Ok"));
+let friend2 ="    Seema    "
+console.log(friend2.trim());
+let fr = "Shivika" + "Riya";
+console.log(fr[0]);
+console.log(fr[1]);
+console.log(fr[2]);
+console.log(fr[3]);
+console.log(fr[4]);
+// QuickQuiz: Use a for loop to print a string
